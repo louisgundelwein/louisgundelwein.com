147afb5c7c3365b9cdbcb7937cf958707b67f3c8
Louis <louis-gundelwein@gmx.de>
2024-05-08 14:37:33 +0200

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
-		<div className="flex flex-col justify-center">
-			<div className="flex flex-row justify-center">
+		<div className="flex flex-col justify-center h-full">
+			<div className="flex flex-row justify-center h-full">
 				<span> Nothing to see here at the moment D:</span>
 			</div>
 		</div>
